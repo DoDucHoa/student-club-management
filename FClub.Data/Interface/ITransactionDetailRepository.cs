8b5f549cc5e230ddd3b0cc0284b12c221d2157b0
Quoc Dung Le <dunglq.contact@gmail.com>
2021-09-24 12:48:43 +0700

    merge

---
+++ FClub.Data/Interface/ITransactionDetailRepository.cs
@@ -0,0 +1,12 @@
+﻿using FClub.Data.Database;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FClub.Data.Interface
+{
+    interface ITransactionDetailRepository : IRepository<TransactionDetail>
+    {
+        void Update(TransactionDetail transactionDetail);
+    }
+}
