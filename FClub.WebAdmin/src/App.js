0692dbc41af8ca5fa69a5cd4ceceb358d3e79746
DoDucHoa <hhoa0978@gmail.com>
2021-09-30 12:42:07 +0700

    Update Web Admin

--- FClub.WebAdmin/src/App.js
+++ FClub.WebAdmin/src/App.js
@@ -6,21 +6,27 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // material UI
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "./Themes/index.js";
+import { CssBaseline } from "@mui/material";
 
 // Link component
 import LoginForm from "./Pages/Authenticate/LoginForm";
 import RegisterForm from "./Pages/Authenticate/RegisterForm";
-import DashBoard from "./Pages/Dashboard/DashBoard";
+import ManageUserForm from "./Pages/ManageUser/ManageUserForm.js";
+import DashBoardLayout from "./Layout/DashBoardLayout.js";
 
 function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
+        <CssBaseline />
         <Router>
           <Switch>
-            <Route exact path="/" component={DashBoard} />
+            <Route exact path="/" component={LoginForm} />
             <Route path="/login" component={LoginForm} />
             <Route path="/register" component={RegisterForm} />
+            <DashBoardLayout>
+              <Route path="/manage-user" component={ManageUserForm} />
+            </DashBoardLayout>
           </Switch>
         </Router>
       </ThemeProvider>
--- FClub.WebAdmin/src/Components/UI/FormCard.js
+++ FClub.WebAdmin/src/Components/UI/FormCard.js
@@ -5,18 +5,20 @@ import React from "react";
 const useStyles = makeStyles((theme) => ({
   root: {
     margin: "10rem auto",
-    width: "30rem",
-    height: "20rem",
-    padding: "5rem",
+    width: "40rem",
+    height: "30rem",
+    padding: "3rem 5rem 5rem 5rem",
+
     [theme.breakpoints.down("md")]: {
-      width: "20rem",
-      height: "23rem",
+      margin: "6rem auto",
+      width: "35rem",
+      height: "31rem",
       padding: "4rem",
     },
     [theme.breakpoints.down("sm")]: {
-      width: "15rem",
-      height: "28rem",
-      padding: "3rem 3rem",
+      width: "20rem",
+      height: "33rem",
+      padding: "2rem 3rem",
       margin: "5rem auto",
     },
   },
