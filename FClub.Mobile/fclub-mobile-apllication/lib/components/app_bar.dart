62d67c7947fad447b0e7529a52ad28692592c5d9
vhnguyen712 <vhnguyen2k@gmail.com>
2021-10-06 20:50:56 +0700

    no message

---
+++ FClub.Mobile/fclub-mobile-apllication/lib/components/app_bar.dart
@@ -0,0 +1,19 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_auth/constants.dart';
+
+// class TitleBar extends StatelessWidget {
+//   final String title;
+//   const TitleBar({
+//     Key? key,
+//     required this.title,
+//   }) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return AppBar(
+//       leading: IconButton(
+//         icon: Image.asset("assets/images/previous.png"
+//       ),
+//     );
+//   }
+// }
