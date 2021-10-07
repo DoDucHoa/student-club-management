eeaeecea317f9793978d1be91db2b09f73760bd8
vhnguyen712 <53395354+vhnguyen712@users.noreply.github.com>
2021-10-07 13:22:43 +0700

    Update PagingParameter.cs

--- FClub.Data/Helper/PagingParameter.cs
+++ FClub.Data/Helper/PagingParameter.cs
@@ -7,7 +7,7 @@ namespace FClub.Data.Helper
     public class PagingParameter
     {
         const int maxPageSize = 50;
-        public int PageNumber { get; set; } = 1;
+        public int PageNumber { get; set; } = 0;
         private int _pageSize = 10;
         public int PageSize
         {
