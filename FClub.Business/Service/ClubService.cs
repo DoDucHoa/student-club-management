2402354dd0977421cf032c81b361c78880406dd9
NgHLong-now <ng.hoang.long10320@gmail.com>
2021-11-04 13:19:39 +0700

    get club for mobile

--- FClub.Business/Service/ClubService.cs
+++ FClub.Business/Service/ClubService.cs
@@ -70,6 +70,86 @@ namespace FClub.Business.Service
             paging.PageSize);
         }
 
+        public PagedList<DictionaryEntry> GetAllClubWithMemberApproved(ClubParameter club, PagingParameter paging, int userId)
+        {
+            var values = _clubRepository.GetAll(includeProperties: club.includeProperties);
+
+            if (!string.IsNullOrWhiteSpace(club.id))
+            {
+                values = values.Where(x => x.Id == club.id);
+            }
+            if (!string.IsNullOrWhiteSpace(club.name))
+            {
+                values = values.Where(x => x.Name.Contains(club.name, StringComparison.InvariantCultureIgnoreCase));
+            }
+            if (!string.IsNullOrWhiteSpace(club.universityID))
+            {
+                values = values.Where(x => x.UniversityId == club.universityID);
+            }
+            if (!club.Status)
+            {
+                values = values.Where(x => x.Status == false);
+            }
+
+            if (!string.IsNullOrWhiteSpace(club.sort))
+            {
+                switch (club.sort)
+                {
+                    case "Id":
+                        if (club.dir == "asc")
+                            values = values.OrderBy(d => d.Id);
+                        else if (club.dir == "desc")
+                            values = values.OrderByDescending(d => d.Id);
+                        break;
+                    case "Name":
+                        if (club.dir == "asc")
+                            values = values.OrderBy(d => d.Name);
+                        else if (club.dir == "desc")
+                            values = values.OrderByDescending(d => d.Name);
+                        break;
+                }
+            }
+
+            var memberWithUserId = _memberService.Get().Where(x => x.UserId == userId);
+
+            Hashtable hashtable = new Hashtable();
+
+            foreach (Club item in values)
+            {
+                hashtable.Add(item, "Not join");
+                var members = item.Members;
+                foreach (var member in members)
+                {
+                    if (memberWithUserId.Contains(member))
+                    {
+                        if (member.IsApproved == false)
+                        {
+                            hashtable[item] =  "Waitting";
+                        }
+                        if (member.IsApproved == true)
+                        {
+                            hashtable[item] = "Joined";
+                        }
+                    }
+                }
+                //if (check == -1)
+                //{
+                    //hashtable.Add(item, "Not join");
+                //}
+            }
+            ICollection key = hashtable.Keys;
+            foreach (var item in key)
+            {
+                Console.WriteLine(item.ToString() + ": " + hashtable[item]);
+            }
+            var list = hashtable.Cast<DictionaryEntry>();
+
+            return PagedList<DictionaryEntry>.ToPagedList(list.AsQueryable(),
+            paging.PageNumber,
+            paging.PageSize);
+            //return list;
+        }
+
         public bool Add(Club club)
         {
             try
